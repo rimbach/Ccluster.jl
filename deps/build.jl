b352ff8a6f062adbefdb6b2d62a5f62a22f7e79d
Remi <imbach@mathematik.uni-kl.de>
2018-08-03 16:29:35 -0400

    win

--- deps/build.jl
+++ deps/build.jl
@@ -7,6 +7,11 @@ wdir = joinpath(pkgdir, "deps")
 vdir = joinpath(pkgdir, "local")
 NemoLibsDir = joinpath(pkgdir, "..", "Nemo", "local")
 
+if is_apple() && !("CC" in keys(ENV))
+   ENV["CC"] = "clang"
+   ENV["CXX"] = "clang++"
+end
+
 if !ispath(vdir)
 
     mkdir(vdir)
@@ -22,6 +27,32 @@ else
 end
 
 # INSTALL CCLUSTER 
+
+cd(wdir)
+
+function download_dll(url_string, location_string)
+   try
+      run(`curl -o $(location_string) -L $(url_string)`)
+   catch
+      download(url_string, location_string)
+   end
+end
+
+if is_windows()
+    if Int == Int32
+        println("No binaries for 32 bits windows yet ... ")
+    else
+        println("downloading binaries ... ")
+        download_dll("https://cims.nyu.edu/~imbach/libs/libgmp-10.dll", joinpath(vdir, "lib", "libgmp-10.dll"))
+        download_dll("https://cims.nyu.edu/~imbach/libs/libmpfr-6.dll", joinpath(vdir, "lib", "libmpfr-6.dll"))
+        download_dll("https://cims.nyu.edu/~imbach/libs/libflint-13.dll", joinpath(vdir, "lib", "libflint-13.dll"))
+        download_dll("https://cims.nyu.edu/~imbach/libs/libarb-2.dll", joinpath(vdir, "lib", "libarb-2.dll"))
+        download_dll("https://cims.nyu.edu/~imbach/libs/libccluster.dll", joinpath(vdir, "lib", "libccluster.dll"))
+    end
+    println("DONE")
+end
+
+cd(wdir)
 
 if !is_windows()
 #   println("Cloning Ccluster ... ")
@@ -43,19 +74,10 @@ if !is_windows()
 #   println("DONE")
 end
 
-if is_windows()
-    if Int == Int32
-        println("No binaries for 32 bits windows yet ... ")
-    else
-    end
-else
-#     println("Building ccluster ... ")
-#     cd(joinpath("$wdir", "arb"))
-#    withenv("LD_LIBRARY_PATH"=>"$vdir/lib", "LDFLAGS"=>LDFLAGS) do
-#       run(`./configure --prefix=$vdir --disable-static --enable-shared --with-mpir=$vdir --with-mpfr=$vdir --with-flint=$vdir`)
-#       run(`make -j4`)
-#       run(`make install`)
-#    end
-#    println("DONE")
-end
+cd(wdir)
+
+push!(Libdl.DL_LOAD_PATH, joinpath(vdir, "lib"))
+
+cd(oldwdir)
+
 #TODO
